3f87c75496d69bc68850fa6caeb08d66fc2c806a
Allegra✨ <amhilburger@gmail.com>
2020-12-05 22:07:52 -0700

    Added Introductory Modal

--- script/app.js
+++ script/app.js
@@ -1,3 +1,5 @@
+console.log($);
+
 //character class
 
 class Character{
@@ -7,22 +9,17 @@ class Character{
     this.healing = healing,
     this.attack = attack
     }
-    // chooseAttack(){
-        
-    // }
-    // chooseHealing(){
 
+    // characterDeath(){
+    //     if(this.lifepoints <= 0){
+    //         prompt(`${this.name} has been defeated by ${Enemy.name}. You have failed Odin and Midgard will be destroyed by Ymir. Frigg, Odins wife has faith in you and is offering to bring you back to life but you must begin again. Replay?, "yes/no"`)
+    //             if(prompt === yes){
+    //                     //restart character modal//
+    //             }else{
+    //                 alert("Game Over");
+    //             }
+    //     }
     // }
-    characterDeath(){
-        if(this.lifepoints <= 0){
-            prompt(`${this.name} has been defeated by ${Enemy.name}. You have failed Odin and Midgard will be destroyed by Ymir. Frigg, Odins wife has faith in you and is offering to bring you back to life but you must begin again. Replay?, "yes/no"`)
-                if(prompt === yes){
-                        //restart character modal//
-                }else{
-                    alert("Game Over");
-                }
-        }
-    }
 }
 const Embla = new Character ("Embla", "100", ["Prayer : 20"], ["Simple Curse: 20"]); ///can I list key value pairs within an array this way?//
 const Ask = new Character ("Ask", "100", ["Leaves: 20"], ["Arrows: 20"]);
@@ -48,13 +45,34 @@ const nokk = new Enemy("Nokk", "River Sprite", "150", "100");
 const stallo = new Enemy("Stallo", "Evil Wizard", "300", "100");
 const goblin = new Enemy("Tusser", "goblin", "175", "75"); 
 const Ymir = new Enemy("Ymir", "giant", "100", "100");
-const modgudr = new Enemey("Modgudr", "maer", "150", "100");
+const modgudr = new Enemy("Modgudr", "maer", "150", "100");
 const Surtr = new Enemy("Surtr", "Fire Giant", "300", "150");
 
 
 
 //introduction modal//  
 
+
+$( () => {
+    //cache DOM nodes//
+const $modal = $("#intro-modal")//modal container
+const $modalText = $("#intro-text")//modal text
+const $openModal = $("#play-btn")//play button starts modal
+const $closeModal = $("#close")//close link to close modal
+
+    //create Event Handlers//
+const openIntro = (event) => {
+    $modal.css("display", "flex");
+}
+const closeIntro = (event) =>{
+    $modal.css("display", "none");
+}
+//create Event Listeners//
+$openModal.on('click', openIntro);
+$closeModal.on('click', closeIntro);
+
+});
+//create Event Handlers//
 
 
 //choose character modal//
